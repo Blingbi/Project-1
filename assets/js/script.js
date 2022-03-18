a08030b257e6c7c603e31d5a9ce1cbb6e5bcc1ba
Markeicegreen16 <98359896+Markeicegreen16@users.noreply.github.com>
2022-03-18 17:54:41 -0400

    Add loop for dates
    Add loop for dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,78 @@
-var stockContainer = document.getElementById('stockData');
-var cryptoContainer =  document.getElementById('cryptoData')
-var fetchButton = document.getElementById('fetch-button');
+$(document).ready(function (){
+  var stockContainer = document.getElementById('stockData');
+  var cryptoContainer =  document.getElementById('cryptoData')
+  var fetchButton = document.getElementById('fetch-button');
+  var today = new Date();
+  var yesterday = new Date();
+  yesterday.setDate(yesterday.getDate()-1)
+  var lastWeek = new Date();
+  lastWeek.setDate(today.getDate()-7);
+  var twoWeeks= new Date()
+  twoWeeks.setDate(today.getDate()-14)
+  var targets = [yesterday,lastWeek,twoWeeks]
+  
+  $(".stockBtn").on("click", function () {
+    var stockName = $(this).siblings(".description").val();
+    targets.forEach(target =>{
+      var dd = String(target.getDate()).padStart(2, '0');
+      var mm = String(target.getMonth() + 1).padStart(2, '0'); //January is 0!
+      var yyyy = target.getFullYear();
+      var dayTitle = yyyy + '-' + mm + '-' + dd;
+      var requestUrl = 'https://api.polygon.io/v1/open-close/'+stockName.toUpperCase()+'/'+dayTitle+'?adjusted=true&apiKey=x9aOGMvupupwhHuYUerXqh9LBf1gm1HN';
+  
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //Using console.log to examine the data
+      console.log(data);
+      console.log(data);
+    
+        console.log("looping");
+        var stockName = document.createElement('h3');
+        var stockOpen = document.createElement('p');
+        var stockClose = document.createElement('p')
 
-var today = new Date();
-var yesterday = new Date(today);
-yesterday.setDate(yesterday.getDate()-1)
+      //Setting the text of the h3 element and p element.
+      stockName.textContent = data.symbol +" "+ today;
+      stockOpen.textContent = "Open: "+data.open;
+      stockClose.textContent = "Close: "+data.close
+
+      //Appending the dynamically generated html to the div associated with the id="users"
+      //Append will attach the element as the bottom most child.
+      stockContainer.append(stockName);
+      stockContainer.append(stockOpen);
+      stockContainer.append(stockClose);
+      })
+    })
+  
+  
+  
+  
+  
+    })
+        
+    // Get nearby values of the description in JQuery
+    
+  
+})
+
+
+
+
+
+
+/*
 var dd = String(yesterday.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 today = yyyy + '-' + mm + '-' + dd;
+oneWeek = yyyy+ '-' + mm + '-' + dd-7;
 document.write(today);
 
     $(document).ready(function () {
-      $(".stockBtn").on("click", function () {
-        
-            // Get nearby values of the description in JQuery
-            var stockName = $(this).siblings(".description").val();
-          var requestUrl = 'https://api.polygon.io/v1/open-close/'+stockName.toUpperCase()+'/'+today+'?adjusted=true&apiKey=x9aOGMvupupwhHuYUerXqh9LBf1gm1HN';
-          
-          fetch(requestUrl)
-            .then(function (response) {
-              return response.json();
-            })
-            .then(function (data) {
-              //Using console.log to examine the data
-              console.log(data);
-              console.log(data);
-            
-                console.log("looping");
-                var stockName = document.createElement('h3');
-                var stockOpen = document.createElement('p');
-                var stockClose = document.createElement('p')
-
-              //Setting the text of the h3 element and p element.
-              stockName.textContent = data.symbol +" "+ today;
-              stockOpen.textContent = "Open: "+data.open;
-              stockClose.textContent = "Close: "+data.close
-
-              //Appending the dynamically generated html to the div associated with the id="users"
-              //Append will attach the element as the bottom most child.
-              stockContainer.append(stockName);
-              stockContainer.append(stockOpen);
-              stockContainer.append(stockClose);
-              })
-            })
-      })
+      
       $(document).ready(function () {
         $(".cryptoBtn").on("click", function () {
           
@@ -76,3 +104,5 @@ document.write(today);
                 })
               })
         })
+
+*/
